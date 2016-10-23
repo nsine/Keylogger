1ad404e976fe51370985bc8dd75cf07a72645a0e
Vlad Klimenkoff <vlad.klimenkoff97@gmail.com>
2016-10-23 16:34:59 +0300

    Initial commit

---
+++ Keylogger/Keylogger.cpp
@@ -0,0 +1,15 @@
+// Keylogger.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+
+int main() {
+	cout << "hello world";
+    return 0;
+}
+
